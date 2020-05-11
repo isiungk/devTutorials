6c951508ad8cac5afa2209be2409fcb8610bc422
iguojun <pm@johnnykwok.com>
2020-05-11 21:54:45 +0800

    remove duplicate 'entry' field in webpack.config.js

--- webpack-demo/OutputManagement/webpack.config.js
+++ webpack-demo/OutputManagement/webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
