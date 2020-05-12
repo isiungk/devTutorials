61e0c72c8d645695d37ec4a4d776ec05d71a5253
iguojun <pm@johnnykwok.com>
2020-05-12 12:21:53 +0800

    completed webpack Guides - Code Splitting - dynamic imports. //https://webpack.js.org/guides/code-splitting/

---
+++ webpack-demo/CodeSplitting/src/index.js
@@ -0,0 +1,15 @@
+async function getComponent() {
+    let element = document.createElement('div');
+
+    const { default: _ } = await import(/* webpackChunkName: 'lodash' */ 'lodash');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+getComponent().then(
+    component => {
+        document.body.appendChild(component());
+    }
+);
